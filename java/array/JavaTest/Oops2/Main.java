b18484fbd555cd55972b7b5add6aef19b4ca8a21
Gaurav Kumar <gk812004@gmail.com>
2026-02-06 11:06:02 +0530

    JavaClassTest

--- java/array/JavaTest/Oops2/Main.java
+++ java/array/JavaTest/Oops2/Main.java
@@ -7,10 +7,15 @@ public class Main {
 
         public static class PercentageCalculation{
             public static void main(String[] args) {
-                Student s1 = new Student("Rahul", 87);
-                Student s2 = new Student("Ananya", 92);
+                Student s1 = new Student("Gaurav", 430);
+                Student s2 = new Student("Abhishek", 367);
                 s1.printPercentage();
                 s2.printPercentage();
+
+
+                System.out.println(s1.info());
+                System.out.println( s2.info());
+
                 System.out.println("College name for all students: " + collegeName);
                 System.out.println("Maximum Marks: " + MAX_MARKS);
             }}}
--- java/array/JavaTest/Oops2/Student.java
+++ java/array/JavaTest/Oops2/Student.java
@@ -5,19 +5,24 @@ class Student {
     double marksObtained;
 
     static String collegeName = "LPU";
-    static final double MAX_MARKS = 100;
+    static final double MAX_MARKS = 500;
 
     public Student(String name, double marksObtained) {
         this.name = name;
         this.marksObtained = marksObtained;
     }
 
+//    @Override
+    public String info() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", marksObtained=" + marksObtained +
+                '}';
+    }
+
     public void printPercentage() {
         double percentage = (marksObtained / MAX_MARKS) * 100;
-        System.out.println("Name: " + name);
-        System.out.println("College: " + collegeName);
-        System.out.println("Marks Obtained: " + marksObtained);
-        System.out.printf("Percentage: %.2f%%\n\n", percentage);
+        System.out.println(name + "'s Percentage: " + percentage + "%");
     }
 
 
